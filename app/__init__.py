a1e65634abaa2a868e4dda955e80549ed6145195
mavedev <usatchoff@gmail.com>
2020-03-27 00:45:41 +0200

    DB connected to the app

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,8 @@ from config import config
 def get_app(config_name: str) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    from .model import db
+    db.init_app(app)
+
     return app
