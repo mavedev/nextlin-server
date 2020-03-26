b34928287a433649b6c817475d5a246ee6b194cf
mavedev <usatchoff@gmail.com>
2020-03-27 00:57:44 +0200

    Origin added

---
+++ app/constants.py
@@ -0,0 +1,3 @@
+_TEXT_SIZE_MAX: int = 255
+_TEXT_SIZE_MID: int = 150
+_TEXT_SIZE_MIN: int = 50
--- app/model.py
+++ app/model.py
@@ -1,3 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
+from .constants import (
+    _TEXT_SIZE_MAX,
+    _TEXT_SIZE_MID,
+    _TEXT_SIZE_MIN
+)
+
 db = SQLAlchemy()
+
+
+class Origin(db.Model):  # type: ignore
+    id = db.Column(db.Integer(), primary_key=True)
+    full_origin = db.Column(db.String(_TEXT_SIZE_MAX), unique=True)
