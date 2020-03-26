bc3ce7d4818ebe815ee23e83383156f4720cd817
mavedev <usatchoff@gmail.com>
2020-03-27 00:43:41 +0200

    Configuration added

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
+from config import config
 
-def get_app() -> Flask:
+
+def get_app(config_name: str) -> Flask:
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
     return app
---
+++ config.py
@@ -0,0 +1,41 @@
+from typing import Dict
+import os
+
+
+class Config:
+    SECRET_KEY: str = os.getenv('SECRET_KEY') or 'default'
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DB_URL_DEV')
+        or 'postgresql+psycopg2://postgres:{password}@localhost/nextlin_dev'
+        .format(password=os.getenv('DB_PASS_DEV') or '')
+    )
+
+
+class TestConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DB_URL_TEST')
+        or 'postgresql+psycopg2://postgres:{password}@localhost/nextlin_test'
+        .format(password=os.getenv('DB_PASS_TEST') or '')
+    )
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DB_URL_PROD')
+        or 'postgresql+psycopg2://postgres:{password}@localhost/nextlin_test'
+        .format(password=os.getenv('DB_PASS_PROD') or '')
+    )
+
+
+config: Dict[str, type] = {
+    'development': DevConfig,
+    'testing': TestConfig,
+    'production': ProdConfig,
+    'default': DevConfig
+}
