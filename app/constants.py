e248d986b80eb21a9c81e725159a522b0a7fe4f5
mavedev <usatchoff@gmail.com>
2020-03-27 01:01:37 +0200

    Origin serialization added

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,8 @@
+from typing import Dict, Any
+
+# Types.
+JSONLike = Dict[str, Any]
+
 _TEXT_SIZE_MAX: int = 255
 _TEXT_SIZE_MID: int = 150
 _TEXT_SIZE_MIN: int = 50
--- app/model.py
+++ app/model.py
@@ -1,9 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
 from .constants import (
+    JSONLike,
     _TEXT_SIZE_MAX,
-    _TEXT_SIZE_MID,
-    _TEXT_SIZE_MIN
 )
 
 db = SQLAlchemy()
@@ -12,3 +11,8 @@ db = SQLAlchemy()
 class Origin(db.Model):  # type: ignore
     id = db.Column(db.Integer(), primary_key=True)
     full_origin = db.Column(db.String(_TEXT_SIZE_MAX), unique=True)
+
+    def serialize(self) -> JSONLike:
+        return {
+            'full_origin': self.full_origin
+        }
