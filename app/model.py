0f3cafc2578170a945cd6ce3d1488999651e1bfe
mavedev <usatchoff@gmail.com>
2020-03-27 01:03:20 +0200

    Writing system added

--- app/model.py
+++ app/model.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from .constants import (
     JSONLike,
     _TEXT_SIZE_MAX,
+    _TEXT_SIZE_MIN,
 )
 
 db = SQLAlchemy()
@@ -15,4 +16,14 @@ class Origin(db.Model):  # type: ignore
     def serialize(self) -> JSONLike:
         return {
             'full_origin': self.full_origin
+        }
+
+
+class WritingSystem(db.Model):  # type: ignore
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(_TEXT_SIZE_MIN), unique=True)
+
+    def serialize(self) -> JSONLike:
+        return {
+            'name': self.name
         }
