55a62e593b26c4026f34486614c8f90f9e870f4b
mavedev <usatchoff@gmail.com>
2020-03-27 01:21:27 +0200

    Language added

--- app/model.py
+++ app/model.py
@@ -49,3 +49,20 @@ class CategoriesRange(db.Model):  # type: ignore
             'min': self.min_value,
             'max': self.max_value
         }
+
+
+class Language(db.Model):  # type: ignore
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(_TEXT_SIZE_MIN), unique=True)
+    has_articles = db.Column(db.Boolean(), default=False)
+    mrph_alignment = db.Column(db.String(_TEXT_SIZE_MIN))
+    dominant_order = db.Column(db.String(_TEXT_SIZE_MIN))
+    writing_system = db.Column(db.String(_TEXT_SIZE_MIN))
+    genders = db.Column(db.Integer())
+    cases = db.Column(db.Integer())
+
+    def serialize(self) -> JSONLike:
+        return {
+            'min': self.min_value,
+            'max': self.max_value
+        }
