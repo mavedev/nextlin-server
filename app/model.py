6cd045a2c904d1217c969d6112e9f61a27ce8e73
mavedev <usatchoff@gmail.com>
2020-03-27 01:24:17 +0200

    Redundant tables removed

--- app/model.py
+++ app/model.py
@@ -9,36 +9,6 @@ from .constants import (
 db = SQLAlchemy()
 
 
-class Origin(db.Model):  # type: ignore
-    id = db.Column(db.Integer(), primary_key=True)
-    full_origin = db.Column(db.String(_TEXT_SIZE_MAX), unique=True)
-
-    def serialize(self) -> JSONLike:
-        return {
-            'full_origin': self.full_origin
-        }
-
-
-class WritingSystem(db.Model):  # type: ignore
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(_TEXT_SIZE_MIN), unique=True)
-
-    def serialize(self) -> JSONLike:
-        return {
-            'name': self.name
-        }
-
-
-class MorphosyntacticAlignment(db.Model):  # type: ignore
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(_TEXT_SIZE_MIN), unique=True)
-
-    def serialize(self) -> JSONLike:
-        return {
-            'name': self.name
-        }
-
-
 class CategoriesRange(db.Model):  # type: ignore
     id = db.Column(db.Integer(), primary_key=True)
     min_value = db.Column(db.Integer())
