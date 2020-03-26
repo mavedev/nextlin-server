1e2a19e20d7437f121f50bb48054aa78dba8dfd8
mavedev <usatchoff@gmail.com>
2020-03-27 01:32:52 +0200

    Migrations added

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 import os
 
 from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 from app import create_app
 from app.model import db
 
 app = create_app(os.getenv('CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 manager.add_command(
     'shell',
     Shell(make_context=lambda: {
